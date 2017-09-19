3f7979a62e5a87f1da58f14e489c1e95632415d5
Alan Davi Ortiz Vargas <alandaviov@gmail.com>
2017-09-19 11:24:21 -0400

    Commit inicial

---
+++ app/src/main/java/com/example/lan/java2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lan.java2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
